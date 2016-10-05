1b7374310781a051509394f60a4a1cf17e5a1166
Krikor Ailanjian <kailanjian@gmail.com>
2016-10-05 12:19:47 -0700

    Cleanup and commenting

--- src/WeatherAPI/WeatherClient.cs
+++ src/WeatherAPI/WeatherClient.cs
@@ -17,46 +17,60 @@ namespace WeatherAPI
             key = Resource.ResourceManager.GetString("APIKey");
         }
 
+        /// <summary>
+        /// Gets data about the weather conditions (currently limited to San Diego)
+        /// </summary>
+        /// <returns>Response with data about the weather</returns>
         public ConditionsResponse GetConditionsResponse()
         {
-
+            // Request URL based on Wunderground API documentation for getting 
+            // city data based on API key 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"http://api.wunderground.com/api/{key}/conditions/q/CA/San_Diego.json");
-            Console.WriteLine($"Request: {request.RequestUri.ToString()}");
             WebResponse response = request.GetResponseAsync().Result;
 
+            // Get JSON data from response
             string json;
-
             using (Stream responseStream = response.GetResponseStream())
             using (StreamReader reader = new StreamReader(responseStream))
             {
                 json = reader.ReadToEnd();
             }
 
+            // Convert JSON
             ConditionsResponse cr = JsonConvert.DeserializeObject<ConditionsResponse>(json);
 
-            Console.WriteLine(cr.current_observation.feelslike_f);
-
+            // Return the CLR data based on the request
             return cr;
         }
 
+        /// <summary>
+        /// Gets the Autocomplete API data from Wunderground
+        /// </summary>
+        /// <param name="query">query to search in the API</param>
+        /// <returns>Response with the appropriate data (list of matching data)</returns>
         public AutocompleteResponse GetAutocompleteResponse(string query)
         {
+            // Based on this page:
+            // https://www.wunderground.com/weather/api/d/docs?d=autocomplete-api&MR=1
             string url = "autocomplete.wunderground.com/aq?query=" + query;
 
+            // Get request
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             WebResponse response = request.GetResponseAsync().Result;
 
+            // Get JSON Data
             string json;
-
             using (Stream responseStream = response.GetResponseStream())
             using (StreamReader reader = new StreamReader(responseStream))
             {
                 json = reader.ReadToEnd();
             }
 
-            AutocompleteResponse cr = JsonConvert.DeserializeObject<AutocompleteResponse>(json);
+            // Convert JSON
+            AutocompleteResponse ar = JsonConvert.DeserializeObject<AutocompleteResponse>(json);
 
-            return cr;
+            // return CLR object
+            return ar;
         }
     }
 }
