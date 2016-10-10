37d08e0c6716e446a95e6a84f929d88f4cf1ab41
Krikor Ailanjian <kailanjian@gmail.com>
2016-10-10 14:47:38 -0700

    Added Constants class to store layout constants

---
+++ src/HomePageApp/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>
+/// Class to hold constants to be used throughout the program. It is especially
+/// useful for allowing constants to be used in the .cshtml files
+/// </summary>
+public class Constants
+{
+
+    public const string BING_URL_BASE = "http://www.bing.com";
+}
