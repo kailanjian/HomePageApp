74699d92e00b6bcd4fa1c956b37dce4310855400
Krikor Ailanjian <kailanjian@gmail.com>
2016-11-19 20:24:06 -0800

    Added search bar functionality

---
+++ src/HomePageApp/Controllers/WidgetsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using WeatherAPI;
+using System.Text;
+
+namespace HomePageApp.Controllers
+{
+    public class WidgetsController : Controller
+    {
+        public IActionResult Search(string query)
+        {
+            string searchURL = "";
+
+            // TODO: logic for search engine options
+            searchURL += Constants.GOOGLE_SEARCH_BASE;
+
+            // append the query
+            searchURL += query;
+
+            return Redirect(searchURL);
+        }
+    }
+}
